0c6d9a7186d8422f11655442f56f11d127f56cbb
itasouza <itasouza@yahoo.com.br>
2020-03-24 13:26:06 -0300

    Adicionado o cadastro de estado

--- MinhaAppMvcCompleta/src/DevIO.App/Data/ApplicationDbContext.cs
+++ MinhaAppMvcCompleta/src/DevIO.App/Data/ApplicationDbContext.cs
@@ -13,8 +13,8 @@ namespace DevIO.App.Data
             : base(options)
         {
         }
-        public DbSet<DevIO.App.ViewModels.ProdutoViewModel> ProdutoViewModel { get; set; }
-       
+        public DbSet<DevIO.App.ViewModels.EnderecoViewModel> EnderecoViewModel { get; set; }
+      
         
 
     }
