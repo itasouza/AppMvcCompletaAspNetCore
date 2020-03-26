92aec265727f61fe5ce755c920e4f760a845916f
itasouza <itasouza@yahoo.com.br>
2020-03-25 23:19:40 -0300

    Adicionado validação para data inicial e final

--- MinhaAppMvcCompleta/src/DevIO.App/wwwroot/js/site.js
+++ MinhaAppMvcCompleta/src/DevIO.App/wwwroot/js/site.js
@@ -1,10 +1,47 @@
 ﻿
 $(document).ready(function () {
     definirDatePicker();
+    
     $('.campovalor').mask('#.##0,00', { reverse: true });
 
 });
 
+
+$("#formBusca").submit(function (event) {
+
+    var data1 = $("#datainicial").val();
+    var data2 = $("#datafinal").val();
+
+    var dataInicial = ConverteParaData(data1);
+    var dataFinal = ConverteParaData(data2);
+
+    if (dataInicial > dataFinal) {
+        alert('A data inicial não pode ser menor que a data final!');
+        event.preventDefault();
+    }
+
+});
+
+
+
+
+function ConverteParaData(data) {
+    var dataArray = data.split('/');
+    var novaData = new Date(dataArray[2], dataArray[1], dataArray[0]);
+    return novaData;
+}
+
+
+function ErroSweetalert() {
+    swal({
+        position: 'top-end',
+        showConfirmButton: false,
+        toast: true,
+        timer: 3000,
+        type: 'error',
+        title: 'A data inicial não pode ser menor que a data final!'
+    });
+}
 
 
 
